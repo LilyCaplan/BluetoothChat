09130ce24d2521401d1089f8a64381113be45aed
Lily Caplan <lilycaplan@Lilys-MacBook-Air.local>
2017-11-04 21:23:55 -0400

    adding connection

--- TechnicaProj2/LAConnectivityManager.m
+++ TechnicaProj2/LAConnectivityManager.m
@@ -5,26 +5,19 @@
 //  Created by Lily Caplan on 11/4/17.
 //  Copyright © 2017 Saad Khalid. All rights reserved.
 //
-
 #import "LAConnectivityManager.h"
-
-@interface LAConnectivityManager ()<MCNearbyServiceBrowserDelegate>
-
+@interface LAConnectivityManager ()<MCNearbyServiceBrowserDelegate, MCSessionDelegate>
 @property(strong, nonatomic) MCNearbyServiceAdvertiser *advertiser;
 @property(strong, nonatomic) MCPeerID *localPeerID;
 @property(strong, nonatomic) MCNearbyServiceBrowser *browser;
-
-
+@property(strong, nonatomic) MCSession *session;
 @end
-
 @implementation LAConnectivityManager
-
 - (instancetype)init {
   NSString * serviceType = @"xoxo";
   self = [super init];
   if (self) {
     self.localPeerID = [[MCPeerID alloc] initWithDisplayName:[[UIDevice currentDevice] name]];
-
     self.advertiser = [[MCNearbyServiceAdvertiser alloc] initWithPeer:self.localPeerID
                                                         discoveryInfo:nil
                                                           serviceType:serviceType];
@@ -36,20 +29,16 @@
   return self;
 }
 #pragma mark - MCNearbyServiceAdvertiserDelegate
-
 - (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser
 didReceiveInvitationFromPeer:(MCPeerID *)peerID
        withContext:(NSData *)context
  invitationHandler:(void(^)(BOOL accept, MCSession *session))invitationHandler{
   NSLog(@"YAY WE'RE CONNECTING: %@", peerID);
   
-  MCSession *session = [[MCSession alloc] initWithPeer:self.localPeerID
-                                      securityIdentity:nil
-                                  encryptionPreference:MCEncryptionNone];
-//  session.delegate = self;
+  MCSession *session = [[MCSession alloc] initWithPeer:self.localPeerID];
+  //  session.delegate = self;
   invitationHandler(YES, session);
 }
-
 - (void)browser:(MCNearbyServiceBrowser *)browser didNotStartBrowsingForPeers:(NSError *)error {
   NSLog(@"Did not start browsing: %@", error);
 }
@@ -57,9 +46,12 @@ didReceiveInvitationFromPeer:(MCPeerID *)peerID
   NSLog(@"Lost Peer: %@", peerID);
 }
 -(void)browser:(MCNearbyServiceBrowser *)browser foundPeer:(MCPeerID *)peerID withDiscoveryInfo:(NSDictionary<NSString *,NSString *> *)info{
-  NSLog(@"Yay connection made with: %@ %@", peerID, info);
-  
+  NSLog(@"Yay peer discovered: %@ %@", peerID, info);
+  self.session = [[MCSession alloc] initWithPeer:self.localPeerID];
+  self.session.delegate = self;
+  [browser invitePeer:peerID toSession:self.session withContext:nil timeout:10];
 }
-
-
+- (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state {
+  NSLog(@"peer: %@, didChangeState: %d", peerID, (int)state);
+}
 @end
